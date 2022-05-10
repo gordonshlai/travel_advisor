ab3aada24ccecd71d800b6f4b16e6b31f3f141d6
Gordon Lai <gordon@builtbypixel.com>
2022-05-10 11:30:57 +0100

    Added mapStyles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,7 +4,7 @@ import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab/Rating";
 
-// import mapStyles from "../../mapStyles";
+import mapStyles from "../../mapStyles";
 import useStyles from "./styles.js";
 
 const Map = ({
@@ -26,13 +26,16 @@ const Map = ({
         center={coordinates}
         defaultZoom={14} // 14 seems to work the best
         margin={[50, 50, 50, 50]}
-        options={""}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
         onChange={(e) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
         onChildClick={(child) => setChildClicked(child)}
-        // options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
       >
         {places?.length &&
           places.map((place, i) => (
